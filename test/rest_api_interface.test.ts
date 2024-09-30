7231cf2388a6c0a5c099579f0d247b0e4316a8a4
MG336 <gm3236@gmail.com>
2024-09-30 13:11:52 +0300

    pull

---
+++ test/rest_api_interface.test.ts
@@ -0,0 +1,9 @@
+import { ApiService } from '../src/projects/rest_api_interface';
+import { describe, it, expect } from 'vitest';
+
+
+describe('ApiService', ()=>{
+    it('getPosts',()=>{
+        
+    })
+})
