50764f86f01c4c288e4df8a058334dfd22c1aca5
MG336 <gm3236@gmail.com>
2024-10-10 19:53:23 +0300

    update generics

--- src/projects/junior/generics.ts
+++ src/projects/junior/generics.ts
@@ -37,12 +37,34 @@ const topItem = stack.peek();
 
 //-------------
 
-interface Response <T, U>{
-     data:T;
-     error: U;
+    interface Response <T, U>{
+        data:T;
+        error: U;
+    }
+
+    const response: Response<{name: string}, null> = {
+        data: {name:'Alice'},
+        error: null
+    }
+
+    function handleResponse<T, U>(response: Response<T, U>): void {
+        if(response.error){
+            console.error('Error:', response.error);
+        }else{
+            console.log('Data:', response.data);
+        }
+    }
+    handleResponse(response);
+
+//-------------
+
+
+function merge<T extends object, U extends object>(obj1: T, obj2: U): T & U{
+    return {...obj1, ...obj2};
 }
 
-function returnResponse<T, U>(data:T, error:U): string{
-    
-}
+const obj1 = { x: 1, y: 2 };
+const obj2 = { y: 3, z: 4 };
+const merged = merge(obj1, obj2);
+
 
