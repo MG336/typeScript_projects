3a7baca2fa4e175cd4b5e7de9b34481a24516606
MG336 <gm3236@gmail.com>
2024-12-22 20:15:02 +0300

    update component_pattern

--- src/projects/middle/design_patterns/component_pattern.ts
+++ src/projects/middle/design_patterns/component_pattern.ts
@@ -3,6 +3,15 @@ interface iProps{
     backgroundColor?:string;
 }
 
+interface iCardProps extends iProps{
+    
+    titleText: string,
+    subTitleText: string,
+
+    titleColor?: string,
+    subTitleColor?: string
+}
+
 interface Component {
     render(container:string):void
     setProps(props: iProps): void
@@ -10,6 +19,7 @@ interface Component {
 
 
 class Button implements Component{
+
     constructor(
         private label: string = 'text',
         private element: HTMLElement = document.createElement('button'),
@@ -25,13 +35,10 @@ class Button implements Component{
     }
 
     public render(container:string){
-        const element = this.element;
-        element.innerText = this.label;
-        element.id = this.id;
         const containerElem = document.getElementById(container);
         
         if(containerElem){
-            containerElem.appendChild(element);
+            containerElem.appendChild(this.element);
         }else{
             console.error('container not found');
         }
@@ -39,13 +46,56 @@ class Button implements Component{
 
     public setProps(props: iProps): void {
         const element = this.element;
-        element.style.color = props.color ?? 'white';
-        element.style.backgroundColor = props.backgroundColor ?? 'black';
+        element.classList.add('button');
+        element.textContent = this.label;
+        element.id = this.id;
+        element.style.color = props.color || 'white';
+        element.style.backgroundColor = props.backgroundColor || 'black';
     }
-
 }
 
+class Card implements Component{
+    constructor(
+        private element: HTMLElement = document.createElement('div'),
+        private id:string = 'card1'
+    ){}
+
+    public setProps(props: iCardProps): void {
+        let element: HTMLElement = this.element;
+        element.classList.add('card');
+        element.id = this.id;
+
+        let div: HTMLElement = document.createElement('div');
+        let title: HTMLElement = document.createElement('H3');
+        let subTitle: HTMLElement = document.createElement('p');
+
+        title.textContent = props.titleText;
+        subTitle.textContent = props.subTitleText;
+        title.style.color = props.titleColor || 'black';
+        subTitle.style.color = props.subTitleColor || 'black';
+
+        div.appendChild(title);
+        div.appendChild(subTitle);
+        
+        element.appendChild(div);
+    }
+
+    public render(container: string): void {
+        const containerElem = document.getElementById(container);
+        
+        if(containerElem){
+            containerElem.appendChild(this.element);
+        }else{
+            console.error('container not found');
+        }
+    }
+}
+
+        
+
+    
 const button = new Button();
+
 button.setProps({
     color: 'red',
     backgroundColor: 'blue'
@@ -53,6 +103,25 @@ button.setProps({
 
 button.onClick(()=>console.log('clicked'));
 button.render('app');
+
+
+const card = new Card();
+
+card.setProps({
+    titleColor:'red',
+    subTitleColor: 'green',
+
+    titleText:'title_1',
+    subTitleText: 'subtitle_1'
+})
+
+card.render('app');
+
+
+        
+
+
+
 
 
 
