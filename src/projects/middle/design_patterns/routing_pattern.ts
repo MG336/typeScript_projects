45990a39acac084e0cfdca43dcdfde222865d527
MG336 <gm3236@gmail.com>
2025-01-13 20:34:44 +0300

    modified routing_pattern.ts

--- src/projects/middle/design_patterns/routing_pattern.ts
+++ src/projects/middle/design_patterns/routing_pattern.ts
@@ -1,21 +1,39 @@
+
+interface Route {
+    path: string;
+    callback: ()=> void            
+}
+
 class Router {
-    private routes: Router[] = [];
+    private routes: Route[] = [];
     private currentRoute: string = '';
 
     constructor(){
-        
+        window.onpopstate = ()=>{
+            this.navigate(window.location.pathname);
+        }
     }
     
-    addRoute(path, callback){
-        this.routes.push({path,callback});
+    addRoute(path: string, callback: ()=> void){
+        
+        this.routes.push({path, callback});
     }
 
-    navigate(path){
-        this.routes.forEach(route => {
-            if(route.path === path){
-                route.callback();
-            }
-        });
+    navigate(path: string){
+        this.currentRoute = path;
+        history.pushState({}, '', path);
+        this.render();
+    }
+
+    render() {
+        const route = this.routes.find(r => r.path === this.currentRoute);
+        if(route){
+            route.callback();
+        } else{
+            console.error('Route not found');
+        }
     }
     
 }
+
+export {Router}
